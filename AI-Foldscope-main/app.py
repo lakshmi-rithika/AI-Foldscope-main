44565042cdbd91d69299e4c16a410cd50b1102be
lakshmi-rithika <rithikalakshmi.b@gmail.com>
2025-12-27 13:55:20 -0800

    Changed App v1

--- AI-Foldscope-main/app.py
+++ AI-Foldscope-main/app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("AI Foldscope Application")
+st.header("Upload and View an Image")
+st.subheader("This application takes a picture as input and analyzes it using AI techniques.")
+
+uploaded_file = st.file_uploader("Chosose an image", type=["png", "jpg", "jpeg", "gif", "bmp"])
+
+if uploaded_file is not None:
+	try:
+		image = Image.open(uploaded_file)
+	except Exception as e:
+		st.error(f"Error opening image: {e}")
+	else:
+		if st.button("View"):
+			st.image(image, width=700)
+			st.write("Uploaded Image:")
